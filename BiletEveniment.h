34a1622fc500cb16eb62b46d7c4fa1cd022abcc8
ToteLorena <120932998+ToteLorena@users.noreply.github.com>
2023-01-12 00:54:40 +0200

    Update BiletEveniment.h

--- BiletEveniment.h
+++ BiletEveniment.h
@@ -2,8 +2,8 @@
 #pragma once
 #include<iostream>
 #include<string>
+#include<cstdlib>.
 #include"Eveniment.h"
-
 
 using namespace std;
 
@@ -22,227 +22,61 @@ private:
 
 public:
 	//CONSTRUCTORI
-	BiletEveniment():identificareBilet(0)
-	{
-		this->tipZona = "Necunoscuta";
-		this->pret = 0;
-		this->nrRand = 0;
-		this->nrLocPeRand = 0;
-		Eveniment e1;
-		this->evenimentBilet = e1;
-		this->esteDecontat = 1;
-	}
+	BiletEveniment();
 
-	BiletEveniment(int identificareBilet, string tipZona, float pret, int nrRand, int nrLocPeRand, Eveniment evenimentBilet, bool esteDecontat) :identificareBilet(identificareBilet)
-	{
-		this->tipZona = tipZona;
-		this->pret = pret;
-		this->nrRand = nrRand;
-		this->nrLocPeRand = nrLocPeRand;
-		this->evenimentBilet = evenimentBilet;
-		this->esteDecontat = esteDecontat;
-	}
+	BiletEveniment(const string tipZona, float pret, int nrRand, int nrLocPeRand, Eveniment evenimentBilet, bool esteDecontat, int identificareBilet);
 
 	//DESTRUCTOR
-	~BiletEveniment()
-	{
-
-	}
+	~BiletEveniment();
 
 	//CONSTRUCTOR DE COPIERE
-	BiletEveniment(BiletEveniment& b):identificareBilet(b.identificareBilet)
-	{
-		this->tipZona = b.tipZona;
-		this->pret = b.pret;
-		this->nrRand = b.nrRand;
-		this->nrLocPeRand = b.nrLocPeRand;
-		this->evenimentBilet = b.evenimentBilet;
-		this->esteDecontat = b.esteDecontat;
-	}
+	BiletEveniment(const BiletEveniment& b);
+
 
 	//OPERATORUL DE ATRIBUIRE
-	BiletEveniment& operator=(BiletEveniment& b)
-	{
-		if (this != &b)
-		{
-			this->tipZona = b.tipZona;
-			this->pret = b.pret;
-			this->nrRand = b.nrRand;
-			this->nrLocPeRand = b.nrLocPeRand;
-			this->evenimentBilet = b.evenimentBilet;
-			this->esteDecontat = b.esteDecontat;
-		}
-		return *this;
-	}
+	BiletEveniment& operator=(const BiletEveniment& b);
 
 	//GETTERI
-	int getIdentificare()
-	{
-		return this->identificareBilet;
-	}
-
-	string getTipZona()
-	{
-		return this->tipZona;
-	}
-
-	float getPret()
-	{
-		return this->pret;
-	}
-
-	int getNrRand()
-	{
-		return this->nrRand;
-	}
-
-	int getNrLocPeRand()
-	{
-		return this->nrLocPeRand;
-	}
-
-	Eveniment getEvenimentBilet()
-	{
-		return this->evenimentBilet;
-	}
-
-	bool getEsteDecontat()
-	{
-		return this->esteDecontat;
-	}
-
-	static int getProcentTVA()
-	{
-		return procentTVA;
-	}
+	int getIdentificare();
+	string getTipZona();
+	float getPret();
+	int getNrRand();;
+	int getNrLocPeRand();
+	Eveniment getEvenimentBilet();
+	bool getEsteDecontat();
+	static int getProcentTVA();
 
 	//SETTERI
-	void setTipZona(string tipZona)
-	{
-		this->tipZona = tipZona;
-	}
-
-	void setPret(float pret)
-	{
-		this->pret = pret;
-	}
-
-	void setNrRand(int nrRand)
-	{
-		this->nrRand = nrRand;
-	}
-
-	void setNrLocPeRand(int nrLocPeRand)
-	{
-		this->nrLocPeRand = nrLocPeRand;
-	}
-
-	void setEvenimentBilet(Eveniment evenimentBilet)
-	{
-		this->evenimentBilet = evenimentBilet;
-	}
-
-	void setEsteDecontat(bool esteDecontat)
-	{
-		this->esteDecontat = esteDecontat;
-	}
-
-	static void setProcentTVA(int procentTVA)
-	{
-		BiletEveniment::procentTVA = procentTVA;
-	}
+	void setTipZona(string tipZona);
+	void setPret(float pret);
+	void setNrRand(int nrRand);
+	void setNrLocPeRand(int nrLocPeRand);
+	void setEvenimentBilet(Eveniment evenimentBilet);
+	void setEsteDecontat(bool esteDecontat);
+	static void setProcentTVA(int procentTVA);
 
 	//OPERATOR <<
-	friend ostream& operator<<(ostream& out, BiletEveniment& b)
-	{
-		out << "Codul pentru identificarea biletului este: " << b.identificareBilet << endl;
-		out << "Zona biletului este: " << b.tipZona << endl;;
-		out << "Pretul biletului este: " << b.pret<<endl;
-		out << "Numarul randului este: " << b.nrRand<<endl;
-		out << "Numarul locului este: " << b.nrLocPeRand<<endl;
-		out << "Detalii despre eveniment: " << b.evenimentBilet<<endl;
-		out << "Este biletul decontat? (1-DA, 0-NU): " << b.esteDecontat<<endl;
-		out << "TVA: " << b.procentTVA<<"%"<<endl;
-		return out;
-	}
+	friend ostream& operator<<(ostream& out, BiletEveniment& b);
 
 	//OPERATOR >>
-	friend istream& operator>>(istream& in, BiletEveniment& b)
-	{
-		cout << "Introduceti zona biletului:";
-		in >> b.tipZona;
-		cout << "Introduceti pretul biletului:";
-		in >> b.pret;
-		cout << "Introduceti numarul randului:";
-		in >> b.nrRand;
-		cout << "Introduceti locul:";
-		in >> b.nrLocPeRand;
-		cout << "Introduceti detaliile despre eveniment:";
-		in >> b.evenimentBilet;
-		cout << "Este biletul decontat? (1-DA, 0-NU):";
-		in >> b.esteDecontat;
-		cout << "Procent TVA:";
-		in >> b.procentTVA;
-		return in;
-	}
+	friend istream& operator>>(istream& in, BiletEveniment& b);
 
 	//METODA STATICA PENTRU CEL MAI IEFTIN BILET
-	static float celMaiIeftinBilet(BiletEveniment* vectorBilete, int nrBilete)
-	{
-		float celMaiIeftinBilet = vectorBilete[0].pret;
-		for (int i = 0; i < nrBilete; i++)
-		{
-			if (celMaiIeftinBilet > vectorBilete[i].pret)
-			{
-				celMaiIeftinBilet = vectorBilete[i].pret;
-			}
-		}
-		return celMaiIeftinBilet;
-	}
-
+	static float celMaiIeftinBilet(BiletEveniment* vectorBilete, int nrBilete);
 
 	//OPERATOR CAST LA FLOAT
 	//PENTRU A CALCULA PRETUL BILETULUI FARA TVA
-
-	operator float()
-	{
-		float pretTVA = 0;
-		pretTVA = this->pret - this->pret*(this->procentTVA / 100);
-		return pretTVA;
-	}
+	operator float();
 
 	//OPERATOR +
 	//PENTRU A CALCULA PRETUL IN CAZUL IN CARE SUNT CUMPARATE MAI MULTE BILETE
-	float operator+(BiletEveniment& b)
-	{
-		float pretTotal = 0;
-		pretTotal = pret + b.pret;
-		return pret;
-	}
+	float operator+(BiletEveniment& b);
 
 	//METODA PRET BILET CU DISCOUNT
-	float aplicareDiscount(float procentDiscount)
-	{
-		float pretDiscount = 0;
-		pretDiscount = this->pret - this->pret * (procentDiscount / 100);
-		return pretDiscount;
-	}
+	float aplicareDiscount(float procentDiscount);
 	
 	//METODA PRET BILET CU ASIGURARE ( IN CAZUL IN CARE EVENIMENTUL SE ANULEAZA/ CUMPARATORUL NU MAI POATE AJUNGE BANII II SUNT RESTITUITI)
-	float pretBiletCuAsigurare(float pretAsigurare)
-	{
-		float pretCuAsigurare = 0;
-		pretCuAsigurare = pret + pretAsigurare;
-		return pretCuAsigurare;
-	}
-	
-
-
-
-
-
-
-	
+	float pretBiletCuAsigurare(float pretAsigurare);
 
 };
-int BiletEveniment::procentTVA = 2;
+
